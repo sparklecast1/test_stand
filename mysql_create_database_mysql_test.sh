dcb7400818bb1666138bcaf309e880c7a6b62fc9
root <root@localhost.localdomain>
2019-05-03 06:43:02 +0500

    3 commit

--- mysql_create_database_mysql_test.sh
+++ mysql_create_database_mysql_test.sh
@@ -12,11 +12,10 @@ CREATE TABLE search (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(100),
   surname VARCHAR(100),
-  email VARCHAR(100),
-  data3 VARCHAR(100),
-  last_update date
+  country VARCHAR(100),
+  last_update date,
+  data VARCHAR(100)
 );
-
 
 
 
@@ -27,21 +26,24 @@ CREATE PROCEDURE InsertRand(IN NumRows INT, IN MinVal INT, IN MaxVal INT)
         SET i = 1;
         START TRANSACTION;
         WHILE i <= NumRows DO
-        SELECT firstname INTO @firstname FROM persons WHERE RAND()<(SELECT ((1/COUNT(*))*10) FROM persons) ORDER BY RAND() LIMIT 1;
-	SELECT lastname INTO @lastname FROM persons WHERE RAND()<(SELECT ((1/COUNT(*))*10) FROM persons) ORDER BY RAND() LIMIT 1;
-            INSERT INTO search (name,surname,email,data3,last_update) VALUES (
+           SELECT firstname INTO @firstname FROM persons WHERE RAND()<(SELECT ((1/COUNT(*))*10) FROM persons) ORDER BY RAND() LIMIT 1;
+           SELECT lastname INTO @lastname FROM persons WHERE RAND()<(SELECT ((1/COUNT(*))*10) FROM persons) ORDER BY RAND() LIMIT 1;
+           SELECT name INTO @country FROM countries ORDER BY RAND() LIMIT 1;
+
+
+            INSERT INTO search (name,surname,country,last_update,data) VALUES (
             @firstname,
             @lastname,
-            MinVal + CEIL(RAND() * (MaxVal - MinVal)),
-            MinVal + CEIL(RAND() * (MaxVal - MinVal)),
-            FROM_UNIXTIME(UNIX_TIMESTAMP('2014-01-01 01:00:00')+FLOOR(RAND()*31536000)));
+            @country,
+            FROM_UNIXTIME(UNIX_TIMESTAMP('2010-01-01 01:00:00')+FLOOR(RAND()*315360000)),
+            MinVal + CEIL(RAND() * (MaxVal - MinVal)));
             SET i = i + 1;
         END WHILE;
         COMMIT;
     END$$
 DELIMITER ;
 
-CALL InsertRand(10000, 12345, 98765);
+CALL InsertRand(1000000, 12345, 98765);
 
 "
 
