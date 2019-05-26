0f62fb1a4dfcb7e5085bd48a6c19476574d9eedf
twy <sparkle.cast@gmail.com>
2019-05-27 04:23:18 +0500

    первая работоспособная версия

--- elastic_importer.py
+++ elastic_importer.py
@@ -2,12 +2,55 @@ from elasticsearch import Elasticsearch
 import requests
 import logging
 import json
+import pymysql.cursors
 
 #connect to our cluster
 #while r.status_code == 200:
     #r = requests.get('http://swapi.co/api/people/'+ str(i))
     #es.index(index='sw', doc_type='people', id=i, body=json.loads(r.content))
     #es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+
+def connect_mysql():
+    #mysql.connector.connect(host='localhost',database='test',user='root',password='123456Aa')
+    connection = pymysql.connect(host='localhost',
+                             user='root',
+                              password='123456Aa',
+                               db='test',
+                                charset='utf8mb4',
+                                cursorclass=pymysql.cursors.DictCursor)
+                                
+    sql = "SELECT * FROM search "
+    #connection = connection.getConnection()
+    cursor = connection.cursor()
+    
+    # Выполнить sql и передать 1 параметр.
+    #cursor.execute(sql, ( 10 ) )
+    cursor.execute(sql)
+    print ("cursor.description: ", cursor.description)
+
+    for row in cursor:
+        data = {
+           "id": row["id"],
+           "name": row["name"],
+           "surname": row["surname"],
+           "country": row["country"],
+           "last_update": row["last_update"],
+           "data": row["data"],
+        }
+        res = es.index(index='search', doc_type='members',  body=data)
+        #return connection
+        #id | name   | surname | country | last_update | data
+        #print(row)
+        #print (" ----------- ")
+        #print("Row: ", row)
+        #print ("id: ", row["id"])
+        #print ("name: ", row["name"])
+        #print ("surname: ", row["surname"])
+        #print ("country: ", row["country"])
+        #print ("last_update: ", row["last_update"])
+        #print ("data: ", row["data"])
+
+
 
 def connect_elasticsearch():
     _es = None
@@ -28,15 +71,24 @@ def create_index(es_object, index_name='search'):
         },
         "mappings": {
             "members": {
-                "dynamic": "strict",
+#                "dynamic": "strict",
                 "properties": {
                     "id": {
                         "type": "integer"
                     },
-                    "id_user": {
-                        "type": "integer"
+                    "name": {
+                        "type": "keyword"
                     },
-                    "text": {
+                    "surname": {
+                        "type": "keyword"
+                    },
+                    "country": {
+                        "type": "keyword"
+                    },
+                    "last_update": {
+                        "type": "date"
+                    },
+                    "data": {
                         "type": "text"
                     },
                 }
@@ -70,16 +122,41 @@ def search(es_object, index_name, search):
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.ERROR)
+    #connect and create index
     es = connect_elasticsearch()
     create_index(es, 'search')
-    data = {
-       'user_id': '1',
-       'test': 'test'
-    }
-    store_record(es, 'search', data)
-    #if es is not None:
-    search_object = {'query': {'match': {'iser_id': '1'}}}
-    se = search(es, 'search', json.dumps(search_object))
-    print(se)
-        
-    
+
+
+    try:
+        connect_mysql()
+        print('Connect to mysql is seccesfull')
+    except Exception as ex:
+        print('Error connecting to mysql')
+
+
+    #data = {
+    #   "user_id": "1",
+    #   "text": "test",
+    #   "id_user": "23123",
+    #}
+
+    #try:
+    #    #res = es.index(index="search", doc_type="members", id=1, body=data)
+    #    res = es.index(index="search",  body=data)
+    #    print('Data importing seccesfull')
+    #except Exception as ex:
+    #    print('Error in import data')
+
+
+
+    if es is not None:
+        search_object = '''{'query': {'match': {'id': '1'}}}'''
+        #se = search(es, 'search', json.dumps(search_object))
+        res = es.search(index="search", body={"query": {'match': {'id': '5'}}})
+        #print(res['hits']['total']['value'])
+        print(res)
+
+
+
+
+
